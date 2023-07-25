68c60b1da67c7d84bba900a0d81b9c1434ad07b3
vjlkof <66524739+vjlkof@users.noreply.github.com>
2023-07-25 13:21:53 -0300

    Adding UI

--- README.md
+++ README.md
@@ -11,4 +11,13 @@ Points to be covered
 [OK] Make your function’s playerSelection parameter case-insensitive.
 [OK] Write a function playRound that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 [OK] Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-[X] At this point you should be using console.log() to display the results of each round and the winner at the end.
+[OK] At this point you should be using console.log() to display the results of each round and the winner at the end.
+
+Second part:
+[OK]Set up a new branch rps-ui on your previous Rock Paper Scissors repo
+[OK]In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+[OK]For now, remove the logic that plays exactly five rounds.
+[OK]Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+[OK]Add a div for displaying results and change all of your console.logs into DOM methods.
+[OK]Display the running score, and announce a winner of the game once one player reaches 5 points.
+[OK] Upload the changes to the main branch
--- script.js
+++ script.js
@@ -50,45 +50,80 @@ function getComputerChoice() {
   }
 }
 
-function getPlayerSelection() {
-  const valueSelected = prompt("Please select Rock, Paper or Scissor");
-  return !valueSelected ? "No selected" : valueSelected.toLowerCase();
-}
-
-function playRound(playerSelection, computerselection) {
+function playRound(playerSelection, computerSelection) {
   switch (playerSelection) {
     case "rock":
-      return checkRock(computerselection);
+      return checkRock(computerSelection);
     case "paper":
-      return checkPaper(computerselection);
+      return checkPaper(computerSelection);
     case "scissor":
-      return checkScissor(computerselection);
+      return checkScissor(computerSelection);
     default:
       return 9;
   }
 }
 
-function game() {
-  let playerResult = 0;
-  let computerResult = 0;
-  for (let i = 0; i < 5; i++) {
-    let result = playRound(getPlayerSelection(), getComputerChoice());
-    console.log(
-      `Round ${i + 1} - player ${
-        !result ? "Draw" : result === 1 ? "Win" : "Lose"
-      }`
-    );
-    playerResult = result === 1 ? playerResult + 1 : playerResult;
-    computerResult = result > 1 ? computerResult + 1 : computerResult;
+let playerResult = 0;
+let computerResult = 0;
+let round = 1;
+let message = null;
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorButton = document.getElementById("scissor");
+
+rockButton.onclick = (event) => gameRound(event);
+paperButton.onclick = (event) => gameRound(event);
+scissorButton.onclick = (event) => gameRound(event);
+
+function gameRound(event) {
+  let result = playRound(event.target.name, getComputerChoice());
+  divResult = document.createElement("div");
+  divResult.className = "round-result";
+  divResult.textContent = `Round ${round} - player ${
+    !result ? "Draw" : result === 1 ? "Win" : "Lose"
+  }`;
+  document.body.append(divResult);
+  playerResult = result === 1 ? playerResult + 1 : playerResult;
+  computerResult = result > 1 ? computerResult + 1 : computerResult;
+
+  if (playerResult > 4) {
+    message = "You won. Congratulations.";
+  }
+  if (computerResult > 4) {
+    message = "You lost. C'mon play again.";
   }
 
-  if (playerResult > computerResult) {
-    console.log("The player won. Congratulations.");
-    return;
+  if (message) {
+    divFinalResult = document.createElement("div");
+    divFinalResult.textContent = message;
+    divFinalResult.className = "final-result";
+    document.body.append(divFinalResult);
+    let playAgainButton = document.createElement("button");
+    playAgainButton.textContent = "Play Again";
+    playAgainButton.className = "play-again";
+    playAgainButton.onclick = playAgain;
+    document.body.append(playAgainButton);
+    rockButton.setAttribute("disabled", "");
+    paperButton.setAttribute("disabled", "");
+    scissorButton.setAttribute("disabled", "");
   }
-  if (playerResult < computerResult) {
-    console.log("The player lost. C'mon play again.");
-    return;
-  }
-  console.log("The player draw. It was hard but try again.");
+
+  round += 1;
+}
+
+function playAgain() {
+  playerResult = 0;
+  computerResult = 0;
+  round = 1;
+  message = null;
+
+  divFinalResult = document.getElementsByClassName("final-result")[0];
+  divFinalResult.remove();
+  buttonPlayAgain = document.getElementsByClassName("play-again")[0];
+  buttonPlayAgain.remove();
+  divResults = document.querySelectorAll('div[class="round-result"]');
+  divResults.forEach((divResult) => divResult.remove());
+  rockButton.removeAttribute("disabled");
+  paperButton.removeAttribute("disabled");
+  scissorButton.removeAttribute("disabled");
 }
